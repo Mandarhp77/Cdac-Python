a592453fb2b7836446ccb7973a5e712229a52a76
Mandar Hemraj Patil <86347341+Mandarhp77@users.noreply.github.com>
2022-11-02 22:03:18 +0530

    Update second_lar.py

--- List/second_lar.py
+++ List/second_lar.py
@@ -1,17 +1,7 @@
 lst = [-1,2,-6,1,7,1,8,8,8]
+lst = list(set(lst))
+lst = sorted(lst)
 
-lst.sort()
-max= max(lst)
-smax=0
-
-for i in range(len(lst)-1,0,-1):
-    print(i)
-    if(i<max):
-        smax=i
-        break
-        
-    
-print("smax", smax)
-    
+print("smax", lst[-2])
 
 
